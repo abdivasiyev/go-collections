78680d38a36d8ddb48baf36e27011279b34da62e
Asliddin Azizovich <asliddin.abdivasiyev@gmail.com>
2022-07-20 19:31:26 +0500

    iterator tests

---
+++ list/array_list_iterator_test.go
@@ -0,0 +1,48 @@
+package list
+
+import "testing"
+
+func TestArrayListIterator_Next(t *testing.T) {
+	l := NewArrayList[int]()
+	l.Add(1)
+	l.Add(2)
+
+	itr := NewArrayListIterator[int](*l)
+
+	if itr.Next() != 1 {
+		t.Errorf("iterator next element must be 1")
+		t.FailNow()
+	}
+
+	if itr.Next() != 2 {
+		t.Errorf("iterator next element must be 2")
+		t.FailNow()
+	}
+}
+
+func TestArrayListIterator_HasNext(t *testing.T) {
+	l := NewArrayList[int]()
+	l.Add(1)
+	l.Add(2)
+
+	itr := NewArrayListIterator[int](*l)
+
+	if itr.HasNext() != true {
+		t.Errorf("iterator must have next element")
+		t.FailNow()
+	}
+
+	itr.Next()
+
+	if itr.HasNext() != true {
+		t.Errorf("iterator must have next element")
+		t.FailNow()
+	}
+
+	itr.Next()
+
+	if itr.HasNext() != false {
+		t.Errorf("iterator must not have next element")
+		t.FailNow()
+	}
+}
